929d641deb189501de63f35b7da9eb55cc5aa83a
ryan lingle <lingleryan@gmail.com>
2021-01-05 15:30:07 -0500

    add list/item togglers

--- app/javascript/src/components/new_list.js
+++ app/javascript/src/components/new_list.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import Context from '../context';
 const maxListsLength = 5;
 
@@ -7,7 +7,17 @@ const NewList = ({ children, id }) => {
     const [show, setShow] = useState(false);
     const lists = state.user.uncreated_lists;
     const maxLists = lists && lists.length === maxListsLength;
+    const menu = useRef();
+
+    useEffect(() => {
+        document.onclick = show ? ({ target }) => {
+            if (target !== menu.current) setShow(false);
+        } : null;
+    }, [show]);
+
     if (lists && lists.length === 0) return <div></div>;
+
+
 
     return(
         <div 
@@ -15,12 +25,13 @@ const NewList = ({ children, id }) => {
             id={id}
             onClick={() => setShow(prev => !prev)} 
         >
-            {children || <i className="fas fa-plus-circle icon-black" style={{fontSize: 'large'}} />}
+            {children || <i className={`fas fa-${show ? 'minus' : 'plus'}-circle icon-black`} style={{fontSize: 'large'}} />}
             {
                 show
 
                 ?   <div 
                         className="new-list-menu" 
+                        ref={menu}
                     >
                         {lists.map((list, i) =>
                             <div key={i} onClick={async () => {
