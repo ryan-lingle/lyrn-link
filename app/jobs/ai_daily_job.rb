c6aa78491a229b4838790c4fa08e0cc9d9f1630b
ryan lingle <lingleryan@gmail.com>
2023-10-08 14:25:42 -0400

    cancel ai daily job for now

--- app/jobs/ai_daily_job.rb
+++ app/jobs/ai_daily_job.rb
@@ -2,10 +2,10 @@ class AiDailyJob < ApplicationJob
     queue_as :default
   
     def perform
-         User.all.each do |user|
-            if user.subscribed?('ai_daily')
-                RecommendationJob.perform_later(user)
-            end
-        end
+        #  User.all.each do |user|
+        #     if user.subscribed?('ai_daily')
+        #         RecommendationJob.perform_later(user)
+        #     end
+        # end
     end
 end
